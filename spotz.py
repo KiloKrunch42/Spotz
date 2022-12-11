dc2882726f44238d9d85d082e8162a295530b886
David <davidlabode@web.de>
2022-12-11 14:23:36 +0100

    add logging

--- spotz.py
+++ spotz.py
@@ -1,17 +1,27 @@
 import sensor
 import mailer
+import pathlib
+from datetime import datetime
 from time import sleep
 import os
 
+def log(text):
+    dir = pathlib.Path(__file__).parent.absolute()
+    logFile = open(str(dir) + "/log.txt", 'a')
+    dt = datetime.now()
+    logFile.write(str(dt) + ": " + str(text) + "\n")
+    logFile.close()
+
 if __name__ == '__main__':
+    log("Spotz started")
     sensor.setup()
     user_config = mailer.read_config(os.getcwd() + '/config.ini')
     while True:
         if sensor.measure() == 1:
+            log("Water sensor triggered")
             mailer.send_message(mailer.create_message(user_config), user_config)
             # We only want to send mails again after the sensor has dried again...
             while sensor.measure() == 1:
                 sleep(300)
-            # TODO: Logging
         # TODO: The sleep value could also be provided per a section in the config.ini
         sleep(300)
